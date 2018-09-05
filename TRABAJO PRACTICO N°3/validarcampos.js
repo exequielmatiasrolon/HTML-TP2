73570b6eea8b1fd04ab19bc4de54c2692285a2e3
Exequiel Matias Rolon Reynoso <kematiasrolon@gmail.com>
2018-09-05 20:56:56 -0300

    al final clase

--- TRABAJO PRACTICO N°3/validarcampos.js
+++ TRABAJO PRACTICO N°3/validarcampos.js
@@ -9,9 +9,30 @@ function validacion(){
 			
 			alert('[ERROR] Error gil de goma');
 			document.getElementById(fied).focus()
+
+
 			return false;
 		}
 
 		return true;
+
+}
+
+	
+function diasemana(){
+
+		field= "input";
+		valor=document.getElementById(field).value;
+
+		var date2= new Date(valor);
+
+		if (date2.getDay()==4) {
+			alert('keko')
+		}
+	
+/* var dias = ["El dia es lununes","El dia es martes","El dia es miercoles","El dia es jueves","El dia es viernes","El dia es sabado","El dia es domingo"];
+alert(dias[date2.getDay()]);
+**/
+
 
 }
