5104fd6fb938a677233514437f935c0530b52fdf
Exequiel Matias Rolon Reynoso <kematiasrolon@gmail.com>
2018-09-06 18:37:57 -0300

    lista cambio de horario

--- TRABAJO PRACTICO N°3/validarcampos.js
+++ TRABAJO PRACTICO N°3/validarcampos.js
@@ -18,16 +18,38 @@ function validacion(){
 
 }
 
+
+function eliminarelementoslista(){
+	var x = document.getElementById("input22");
+    x.remove;
+}
+
+function agregarelementoslista(hora){
+
+	var x = document.getElementById("input22");
+    var option = document.createElement("option");
+    option.text = hora
+    x.add(option)     
+
+}
+
 	
 function diasemana(){
 
 		field= "input";
 		valor=document.getElementById(field).value;
+		var hora1 = "22:00"
+		var hora2 = "22:30"
 
 		var date2= new Date(valor);
 
 		if (date2.getDay()==4) {
-			alert('keko')
+			alert('El dia que selecciono es VIERNES por lo tanto tendra 2 horarios mas')
+		 agregarelementoslista(hora1)
+		 agregarelementoslista(hora2)
+
+    
+
 		}
 	
 /* var dias = ["El dia es lununes","El dia es martes","El dia es miercoles","El dia es jueves","El dia es viernes","El dia es sabado","El dia es domingo"];
